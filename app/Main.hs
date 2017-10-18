d4c030b855e209a36bc3a2f507f41683dc1d7895
RyanBeatty <rvbeatty@email.wm.edu>
2017-10-18 17:16:30 -0500

    added documentation

--- app/Main.hs
+++ app/Main.hs
@@ -22,6 +22,7 @@ import GHC.Generics (Generic)
 import Network.Transport.TCP (createTransport, defaultTCPParameters)
 import System.IO (hSetBuffering, stdout, BufferMode(..))
 
+-- A Tick message will be sent by the ticker process to its parent process for timeouts.
 data Tick = Tick
   deriving (Show, Generic, Typeable)
 instance Binary Tick
@@ -53,7 +54,6 @@ newtype ServerAction m a = ServerAction { runAction :: RWST ServerConfig [Messag
 
 type ProcessAction a = ServerAction Process a
 
-
 -- Event-loop of the Controller process. Will poll stdinput for commands
 -- issued by the user until told to quit.
 runController :: Process ()
@@ -83,11 +83,15 @@ spawnServer = do
   spawnLocal $ do
     my_pid <- getSelfPid
     say $ "Spawned process: " ++ (show my_pid)
+    -- Get list of peers before starting to serve.
     peers <- expect :: Process [ProcessId]
+    -- Create ticker and link the ticker process to this process so
+    -- that is will shutdown when this process terminates.
     ticker_pid <- spawnTicker my_pid
     link ticker_pid
-    return ()
 
+-- Create a ticker process that will periodically send ticks to
+-- the parent server.
 spawnTicker :: ProcessId -> Process ProcessId
 spawnTicker parent_pid = spawnLocal $ forever $ do
   send parent_pid Tick
